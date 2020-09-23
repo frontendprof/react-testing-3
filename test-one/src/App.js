c9e42b7e3402587fcf59acbca37d5abc60a23ad8
frontendprof <abdumaliksharipov@gmail.com>
2020-09-23 22:02:28 +0500

    Manipulating the state

--- test-one/src/App.js
+++ test-one/src/App.js
@@ -8,10 +8,18 @@ class App extends Component {
 
 state={
   persons:[
+    {name:"Abula", age:"22"},
+    {name:"Soxi", age:"21"}
+  ]
+}
+
+switchNameHandler=()=>{
+  this.setState({persons:[
     {name:"AbdulMalik", age:"32"},
     {name:"Sohiba", age:"31"},
-    {name:"Sumayya", age:"5"},
-  ]
+    // {name:"Kid", age:null}
+
+  ]})
 }
 
 
@@ -19,10 +27,12 @@ state={
     return (
       <div>
         <h2>Greetings</h2>
+        <br></br>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <br></br>
 
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
        
         
       </div>
