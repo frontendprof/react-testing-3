03128bf7f9495e9a4479235bfe192cb02816cec7
frontendprof <abdumaliksharipov@gmail.com>
2020-09-24 10:08:27 +0500

    Rendering content conditionally

--- test-one/src/App.js
+++ test-one/src/App.js
@@ -9,15 +9,17 @@ class App extends Component {
 state={
   persons:[
     {name:"Abula", age:"22"},
-    {name:"Soxi", age:"21"}
-  ]
+    {name:"Soxi", age:"21"},
+    {name:"Sumi",age:"1"}
+  ],
+  showPersons:false
 }
 
 switchNameHandler=()=>{
   this.setState({persons:[
     {name:"AbdulMalik", age:"32"},
     {name:"Sohiba", age:"31"},
-    // {name:"Kid", age:null}
+    {name:"Sumayya",age:"5"}
 
   ]})
 }
@@ -25,9 +27,17 @@ switchNameHandler=()=>{
 nameChangeHandler =(e)=>{
   this.setState({persons:[
     {name:"AbdulMalik", age:"32"},
-    {name:e.target.value.toUpperCase(), age:"31"}
+    {name:e.target.value.toUpperCase(), age:"31"},
+    {name:"Sumayya", age:"5"}
 
   ]})
+}
+
+
+togglePersonsHandler=()=>{
+
+  const doesShow=this.state.showPersons;
+  this.setState({showPersons:!doesShow});
 }
 
 
@@ -50,19 +60,33 @@ nameChangeHandler =(e)=>{
         <br></br>
         <button 
         style={style}
-        onClick={this.switchNameHandler}>Switch Name</button>
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
         <br></br>
 
-        <Person name={this.state.persons[0].name}
-         age={this.state.persons[0].age}
-         click={this.switchNameHandler}
 
-         />
-        <Person name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        changed={this.nameChangeHandler}
+        {
+          this.state.showPersons?
+            <div>
+            <Person name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler}
+            />
 
-        />
+            <Person name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            changed={this.nameChangeHandler}
+            />
+
+            <Person name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}
+            changed={this.nameChangeHandler}
+            />
+
+          </div>:null
+
+        }
+
+         
        
         
       </div>
